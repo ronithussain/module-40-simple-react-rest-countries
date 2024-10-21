1f90379cd1c92f1b2afbd781ddcf6dda973ece0b
ronithussain <xayaanhussain542@gmail.com>
2024-10-21 19:25:33 +0600

    added my countries

--- src/components/countryData/CountryData.jsx
+++ src/components/countryData/CountryData.jsx
@@ -5,7 +5,7 @@ const CountryData = ({country, handleVisitedCountry, handleVisitedFlag}) => {
     console.log('inside country data',country,handleVisitedCountry,handleVisitedFlag)
     return (
         <div>
-            <h4><small>Country Data:{country.name.common}</small></h4>
+            <h4><small>Countries:{country.name.common}</small></h4>
         </div>
     );
 };
